3e04ab78892f2d90e1d188498f3c7e215ddfaeef
yourUsername <your_email@example.com>
2017-01-13 14:53:20 -0500

    heroku and mongolab setup

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+'use strict';
 // server.js
 
 // BASE SETUP
 // =============================================================================
-
+var Beer = require('./app/models/beer')
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');
+// call express
+var app        = express();
+// define our app using express
 var bodyParser = require('body-parser');
 
-
+var mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/beer_api')
 // set up a variable to hold our model here...
+mongoose.Promise = global.Promise;
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080;
+// set our port
+
+
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
+// get an instance of the express Router
 
 router.use(function(req, res, next) {
   console.log("Something is happening");
@@ -25,7 +36,7 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the beer api!' });   
+  res.json({ message: 'Welcome to the beer api!' });
 });
 
 // more routes for our API will happen here
@@ -33,20 +44,23 @@ router.route('/beers')
 
 // create
   .post(function(req, res) {
-    // code here
+    Beer.create(req.body.beer)
+  .then((beer) => {res.json(beer);})
+  .catch((err) => {if(err) console.log(err);});
   })
 
 // index
   .get(function(req, res) {
-    // code here    
-  });
-
+Beer.find().then(function(beers){res.json(beers);});
+ });
 
 router.route('/beers/:beer_id')
 
   // show
   .get(function(req, res) {
-    // code here
+    Beer.findOne({_id: req.body.beer_id})
+    .then((beer) => { res.json(beer);})
+    .catch((err) => { if(err) console.log(err);});
   })
 
   // update
